03fe11474dc957eab332ba36db88c2512ae67745
iannakim <dear.hyunji@gmail.com>
2021-02-22 10:44:48 -0500

    Validation added

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :article
+
+  validates :content, length: { maximum: 1000,
+    too_long: "%{count} characters is the maximum allowed" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :activities, dependent: :destroy
+  has_secure_password
+
+  validates :firstname, :lastname, :email, :password, presence: true
+  validates :firstname, format: { with: /\A[a-zA-Z]+\z/, message: "please use letters only" }
+  validates :lastname, format: { with: /\A[a-zA-Z]+\z/, message: "please use letters only" }
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { in: 6..20 }
+
 end
